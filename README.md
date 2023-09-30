6d6ea4ccde449eed6dc40e63ab3518bc2599866a
Pere Giménez <gimenezfebrer@gmail.com>
2023-09-30 17:50:01 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,33 @@
+## Histogram demo
+
+Upload a CSV file and plot a histogram from one of its columns.
+
+## Installation
+
+Clone the repository and install the dependencies:
+
+First `cd` into the project directory then run:
+
+```bash
+$> julia --project -e 'using Pkg; Pkg.instantiate()'
+```
+
+Then run the app
+
+```bash
+$> julia --project
+```
+
+```julia
+julia> using GenieFramework
+julia> Genie.loadapp() # load app
+julia> up() # start server
+```
+
+## Usage
+
+Open your browser and navigate to `http://localhost:8000/`
+
+---
+
+Alternatively, register/login at <https://geniecloud.app> and import the app. You can then deploy it to the cloud.
---
+++ ui.jl
@@ -0,0 +1,33 @@
+[
+    heading("{{title}}")
+    row([
+        cell(class="col-md-12", [
+            uploader(label="Upload Dataset", accept=".csv", multiple=true, method="POST", url="http://localhost:8000/", field__name="csv_file", @on("uploaded", :uploaded))
+        ])
+    ])
+    row([
+        cell(
+            class="st-module",
+            [
+                h6("File")
+                Stipple.select(:selected_file; options=:upfiles)
+            ]
+        )
+        cell(
+            class="st-module",
+            [
+                h6("Column")
+                Stipple.select(:selected_column; options=:columns)
+            ]
+        )
+    ])
+    row([
+        cell(
+            class="st-module",
+            [
+                h5("Histogram")
+                plot(:irisplot)
+            ]
+        )
+    ])
+] |> string
